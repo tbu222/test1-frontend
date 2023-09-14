935f2ae08a9b52c638dc91180b7806db293fda97
thinh <thinhbui2911@gmail.com>
2023-09-14 15:05:44 -0700

    test

--- src/components/Upload.styled.js
+++ src/components/Upload.styled.js
@@ -2,15 +2,14 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
-    height: 100%;
+    height: 100vh;
     position: absolute;
     top: 0;
     left: 0;
-    background-color: #000000a7;
-    display: flex;
     align-items: center;
     justify-content: center;
-    z-index:10;
+    display: flex;
+    gap: 24px;
 `;
 
 export const Wrapper = styled.div`
